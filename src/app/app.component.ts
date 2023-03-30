0e7b1cc7b15ead236bb95a6f779a7a39577814b7
Jacek Grochowina <jacek99@vp.pl>
2023-03-30 21:36:23 +0200

    add warnings for moment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MomentLang } from "./shared/enums/moment-lang.enum";
-
-// Moment.js - select language
-import * as moment from "moment";
-import 'moment/locale/pl';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    moment.locale(MomentLang.polish); // Moment.js - select language
-  }
-}
+export class AppComponent {}
