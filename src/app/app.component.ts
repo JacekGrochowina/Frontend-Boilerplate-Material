d972fd9b9857d380fa6db1cdd9060a414a3f41dd
Jacek Grochowina <jacek99@vp.pl>
2023-03-30 21:20:24 +0200

    add moment library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MomentLang } from "./shared/enums/moment-lang.enum";
+
+// Moment.js - select language
+import * as moment from "moment";
+import 'moment/locale/pl';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    moment.locale(MomentLang.polish); // Moment.js - select language
+  }
+}
